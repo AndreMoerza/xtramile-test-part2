78ea2c7937b383c0e28d9db111718d2f8aa9caec
DESKTOP-C4PTAPM\Andre Pratama <andre.moerza@gmail.com>
2024-05-06 20:18:17 +0800

    Added and completed backend

---
+++ Backend/XtramileWeather/Services/XtramileWeatherService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+using XtramileWeather.Model;
+using Microsoft.Extensions.Logging;
+using XtramileWeather.Controllers;
+using System.Threading.Tasks;
+using XtramileWeather.Helper;
+using Microsoft.Extensions.Configuration;
+
+namespace XtramileWeather.Services
+{
+    public class XtramileWeatherService : IXtramileWeatherService
+    {
+        private readonly ILogger<XtramileWeatherService> _logger;
+        private readonly IConfiguration _config;
+
+
+        public XtramileWeatherService(ILogger<XtramileWeatherService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _config = configuration;
+        }
+
+        public async Task<WeatherResponse> GetWeather(string city)
+        {
+            string apiKey = _config["AppSettings:ApiKey"];
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Get,
+                        RequestUri = new Uri($"https://api.openweathermap.org/data/2.5/weather?q={city}&APPID={apiKey}"),
+                    };
+
+                    using (var response = await httpClient.SendAsync(request))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        var weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(apiResponse);
+
+                        weatherResponse.Time = DateTime.UtcNow.AddSeconds(weatherResponse.Timezone);
+                        weatherResponse.TemperatureConverter();
+                        return weatherResponse;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return new WeatherResponse() { ErrorMessage = ex.Message };
+            }
+        }
+    }
+}
---
+++ Backend/XtramileWeather/Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System;
+using XtramileWeather.Services;
+
+namespace XtramileWeather
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+            services.AddControllersWithViews();
+            services.AddMvc();
+
+            services.AddSingleton<IXtramileWeatherService, XtramileWeatherService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseCors(builder => builder
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowAnyOrigin()
+             );
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Xtramile Weather");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
